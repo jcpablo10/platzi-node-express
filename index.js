5b969fe453f35771696b6f2931e8f7d800f43488
Juan C Pablo <jcpablo10@gmail.com>
2022-01-30 15:24:27 -0600

    Using post with express.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routerApi = require('./routes')
 const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola!, mi app esta funcinando');
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
+const express = require('express');
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/users', usersRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+
+  // v2 ?
+  // const router = express.Router();
+  // app.use('/api/v1', router);
 }
 
 module.exports = routerApi;
