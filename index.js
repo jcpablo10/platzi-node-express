baa735b11fc76a63a048b9ade2001c1b25fdd398
Juan C Pablo <jcpablo10@gmail.com>
2022-02-17 00:22:07 -0600

    Manage CORS

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routerApi = require('./routes');
 const { errorHandler, logErrors, boomErrorHandler} = require('./middleware/error.handler.js');
 const port = 3000;
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500', 'http://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola!, mi app esta funcinando');
